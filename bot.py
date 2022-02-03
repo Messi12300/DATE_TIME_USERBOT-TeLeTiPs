d68cdbcc42850581636f5305f69f2e7f830f7c63
Messi12300 <87484413+Messi12300@users.noreply.github.com>
2022-02-03 19:45:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters, idle
 api_id = int(os.environ["API_ID"]),
 api_hash = os.environ["API_HASH"],
 session_name = os.environ["SESSION_NAME"]
-BOT_TOKEN = environ.get("BOT_TOKEN")
+BOT_TOKEN = environ.get("5039475558:AAGw4hIpCIfeoV3wyBerCwsdQNnenTAPd0o")
 TIME = int(environ.get("1800"))
 GROUPS = []
 for grp in environ.get("-1001530674974").split():
@@ -17,7 +17,7 @@ for usr in environ.get("1154056577").split():
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
 
 
-User = Client(session_name=SESSION,
+User = Client(session_name=session_name,
               api_id=api_id,
               api_hash=api_hash,
               workers=300
